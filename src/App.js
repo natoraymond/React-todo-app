98b1214040aebab0b33502d61bf725c1aabb2694
Your Name <nnamdi.raymondoct@gmail.com>
2024-03-21 05:18:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+// import  './App.css'
+import './Assets/style.css';
+
+
+
+function App() {
+  return (
+
+    <div className='todo-app'>
+      {/* <TodoForm /> */}
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
